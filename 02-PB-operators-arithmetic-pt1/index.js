d6c8d5324c95b1eee03a815637f9118840fa6e27
Blash <frostbittenblash@gmail.com>
2020-05-13 15:06:15 +0200

    done

---
+++ 02-PB-operators-arithmetic-pt1/index.js
@@ -0,0 +1,47 @@
+let x = 12;
+let y = 3;
+
+console.log(typeof (x));
+
+console.log(x + y);
+console.log((x - y), (y - x));
+console.log(x - y);
+console.log(y - x);
+console.log(x * y);
+console.log(x / y);
+
+let z = 10;
+
+//Task 5 
+
+let resultOne = (x * y) / z;
+console.log(resultOne);
+
+//Task 5.1
+
+let a = 15;
+let b = 9;
+console.log(a / b);
+
+//Task 6
+
+let c = 20;
+let resultTwo = (a + b) * c;
+console.log(resultTwo);
+
+//Task 7
+a++;
+console.log(a);
+
+//Task 8
+b--;
+console.log(b);
+
+//Task 9
+
+let d = b - a;
+console.log(d + c)
+
+//Task 10
+
+console.log(resultOne % resultTwo)
