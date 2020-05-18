7e2ca05c617a1739a747991e60ec47e38ff729ff
Blash <frostbittenblash@gmail.com>
2020-05-18 12:57:28 +0200

    exercise 05 done

--- 05-PB-operators-comparison/test.js
+++ 05-PB-operators-comparison/test.js
@@ -1,14 +1,44 @@
 'use strict';
 
 let x = 20;
-let y = 15;
-let z = 10;
+let y = 30;
 
-console.log('Answer 8:');
-console.log(x === y === z);
-
+/* console.log('Answer 8:');
+ */
 // remainder % modulo 
 
 // Ternary 
 // condition ? console.log("hello") : console.log("goodbye");
 //condition ? true : false; 
+
+// Task 1
+console.log("Q1:", x === y);
+
+// Task 2
+console.log("Q2:", x != y);
+
+// Task 3
+console.log("Q3:", x > y);
+
+// Task 4
+console.log("Q4:", x <= y);
+
+// Task 5
+console.log("Q5:", y > x);
+
+// Task 6
+let z = 5;
+console.log("Q6:", (z * x) > (y + z));
+
+// Task 7 
+console.log("Q7:", (x - z) < (y / z));
+
+// Task 8 
+console.log("Q8:", x === y === z);
+
+// Task 9 
+console.log("Q9:", (x % z) === (y / z));
+
+// Task 10
+console.log("Q10.1:", (x + z) > (y - z));
+console.log("Q10.2:", (x * z) > (y - z)); 
