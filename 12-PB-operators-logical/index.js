20b32bf3f386576b7d36ad3640760a7d9afff2b3
AndreaBLS <frostbittenblash@gmail.com>
2020-05-25 12:20:05 +0200

    added comments

--- 12-PB-operators-logical/index.js
+++ 12-PB-operators-logical/index.js
@@ -4,9 +4,9 @@ let a = true;
 let b = false;
 
 // Task 1
-console.log("1a:", a && b);
-console.log("1b:", a || b);
-console.log("1c:", !(a && b));
+console.log("1a:", a && b); // False as a && b would need to be both True.
+console.log("1b:", a || b); // True as on them (a) is True!
+console.log("1c:", !(a && b)); // no idea!
 
 // Task 2 
 let x = 3;
@@ -14,9 +14,9 @@ let y = 2;
 let z = 1;
 
 // Task 3
-console.log("3a:", x > z && x > y);
-console.log("3b:", x != y);
-console.log("3c:", z < y || z > x);
-console.log("3d:", x == z || x != y);
-console.log("3e:", x >= 10 && y <= 10);
-console.log("3f:", x * z < 100 || x * y > 100);
+console.log("3a:", x > z && x > y); // True as is X is greater than both Z & Y
+console.log("3b:", x != y); // True as X & Y are not equals
+console.log("3c:", z < y || z > x); // True as Z is less than Y
+console.log("3d:", x == z || x != y);  // True because even if X is not Equal to Z, X is not equal to Y so the needed condition of one True is present.
+console.log("3e:", x >= 10 && y <= 10); // False because neither condition is met.
+console.log("3f:", x * z < 100 || x * y > 100); // True because X * Z is less than 100, so atleats one condition is met.
