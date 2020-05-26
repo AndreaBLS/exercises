79f9340fd7bbba08d404f9a512f889963de63c27
AndreaBLS <frostbittenblash@gmail.com>
2020-05-26 09:58:24 +0200

    added response to Readme line 8 as requested

--- 13-PB-language-boolean/index.js
+++ 13-PB-language-boolean/index.js
@@ -5,6 +5,10 @@ console.log(3 === "3"); // false because 3 is a number and "3" is a string, whic
 console.log(3 == "3"); // true because here we used the "loose" equality, which means we do not check the data type
 /* console.log(3 = "3"); SyntaxError: Invalid left-hand side in assignment  */
 
+/* Which comparison operator should we generally use? Why?
+We generally use "===" because it also checks that data type matches */
+
+
 // Task 2
 var myTrue = true;
 console.log(!myTrue ? true : false);
