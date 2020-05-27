c9d9168d876001ce0f429548ed2a2c70ea683be4
AndreaBLS <frostbittenblash@gmail.com>
2020-05-27 11:40:14 +0200

    easily readable now

--- 16-PB-conditionals-pt2/index.js
+++ 16-PB-conditionals-pt2/index.js
@@ -1,3 +1,8 @@
+
+/* # Programming Basics: Conditionals Part 2
+
+Andrea & Willy are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height). (mass in kg, i.e. 72.5, and height in metres, i.e. 1.65). */
+
 "use strict";
 
 function BMIcalc(mass, height) {
@@ -6,16 +11,20 @@ function BMIcalc(mass, height) {
     return bmi;
 }
 
+// 1. Store Mark's and John's mass and height in variables.
 let andrea = { firstName: "Andrea", mass: 72, age: 29, height: 1.75 };
 let willy = { firstName: "Willy", mass: 85, age: 30, height: 1.69 };
 
+// 2. Calculate both their BMIs and store their BMIs in variables. 
 let andreaBMI = BMIcalc(andrea.mass, andrea.height);
 let willyBMI = BMIcalc(willy.mass, willy.height);
 console.log(`Andrea's BMI is ${andreaBMI} and Willy's BMI is ${willyBMI}`)
 
+// 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
 let higherBMI;
 if (willyBMI > andreaBMI) {
     higherBMI = true;
+    //    4. Print a string to the console containing the variable from step 3 using string interpolation. 
     console.log(`\nIs Willy's BMI higher than Andrea's? ${higherBMI}\n`)
 } else {
     higherBMI = false;
@@ -25,6 +34,7 @@ if (willyBMI > andreaBMI) {
 let message = {
     higherBMI: "Has the higher BMI"
 }
+// 5. Create an *if* statement which prints the name and BMI of the person with the highest BMI. 
 
 if (willyBMI > andreaBMI) {
     console.log(willy.firstName, message.higherBMI);
