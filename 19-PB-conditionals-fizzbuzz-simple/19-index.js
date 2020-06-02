6d3a1dc2f5de17fda120db5ff53739e5150a8ae2
AndreaBLS <frostbittenblash@gmail.com>
2020-06-02 09:03:28 +0200

    semicolumns... :)

--- 19-PB-conditionals-fizzbuzz-simple/19-index.js
+++ 19-PB-conditionals-fizzbuzz-simple/19-index.js
@@ -5,7 +5,7 @@ let number = 3;
 if (number % 3 === 0) {
     console.log("Fizz");
 } else if (number % 5 === 0) {
-    console.log("Buzz")
+    console.log("Buzz");
 } else if ((number % 3) === 0 && (number % 5) === 0) {
     console.log("Fizzbuzz");
 } else {
