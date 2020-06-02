b4f7b8a30f98a28c5a073977b532b316e58e5f99
AndreaBLS <frostbittenblash@gmail.com>
2020-06-02 11:22:07 +0200

    fixed ex 19

--- 19-PB-conditionals-fizzbuzz-simple/19-index.js
+++ 19-PB-conditionals-fizzbuzz-simple/19-index.js
@@ -2,12 +2,13 @@
 
 let number = 3;
 
-if (number % 3 === 0) {
+
+if ((number % 3) === 0 && (number % 5) === 0) {
+    console.log("Fizzbuzz");
+} else if (number % 3 === 0) {
     console.log("Fizz");
 } else if (number % 5 === 0) {
     console.log("Buzz");
-} else if ((number % 3) === 0 && (number % 5) === 0) {
-    console.log("Fizzbuzz");
 } else {
     console.log(number);
 } 
