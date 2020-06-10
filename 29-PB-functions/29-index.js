a40474d928fde39cb18dc583db600ec829eb45af
AndreaBLS <frostbittenblash@gmail.com>
2020-06-10 10:00:46 +0200

    improvements to my solutions, mainly default values

--- 29-PB-functions/29-index.js
+++ 29-PB-functions/29-index.js
@@ -14,42 +14,36 @@ console.log(addUp(13));
 console.log(addUp(600));
 
 // Task 2 
-const sumOfCubes = (numberOne, numberTwo, numberThree) => {
-    const cubeOne = Math.pow(numberOne, 3),
-        cubeTwo = Math.pow(numberTwo, 3),
-        cubeThree = Math.pow(numberThree, 3);
-    return cubeOne + cubeTwo + cubeThree;
+const sumOfCubes = (numberOne = 0, numberTwo = 0, numberThree = 0) => {
+    return Math.pow(numberOne, 3) + Math.pow(numberTwo, 3) + Math.pow(numberThree, 3)
 }
 console.log("Task 2:");
 console.log(sumOfCubes(1, 5, 9));
-console.log(sumOfCubes()); // I get "NaN", should I use an if/else in order to  return "0", in case there are no numbers as inputs?
+console.log(sumOfCubes(2));
+console.log(sumOfCubes());
 
 // Task 3
-const dictionary = (string, word) => {
-    const startChecker = word.startsWith(string);
-    return startChecker;
-}
+const dictionary = (string, word) => word.startsWith(string);
+
 console.log("Task 3:");
 console.log(dictionary("bu", "button"));
 console.log(dictionary("tri", "triplet"));
 console.log(dictionary("beau", "pastry"));
 
 // Task 4
-const lessThanOrEqualToZero = (number) => {
-    let numberChecker = null;
+const lessThanOrEqualToZero = (number = 0) => {
     if (number <= 0) {
-        numberChecker = true;
+        number = true;
     } else {
-        numberChecker = false;
+        number = false;
     }
-    return numberChecker;
+    return number;
 }
 console.log("Task 4:");
 console.log(lessThanOrEqualToZero(3));
 console.log(lessThanOrEqualToZero(0));
 console.log(lessThanOrEqualToZero(-4));
 console.log(lessThanOrEqualToZero(10));
-/* console.log(lessThanOrEqualToZero(3),lessThanOrEqualToZero(0), lessThanOrEqualToZero(-4), lessThanOrEqualToZero(10));  WHICH ONE LOOKS NICER??*/
 
 // Task 5
 const countOccurrences = (string, letter) => {
@@ -63,7 +57,7 @@ console.log(countOccurrences("andrea", "a"));
 console.log(countOccurrences("this is a string", "o"));
 
 // Task 6 
-const calculateBaseToExponent = (baseNumber, exponentNumber) => {
+const calculateBaseToExponent = (baseNumber = 0, exponentNumber = 0) => {
     return baseNumber ** exponentNumber;
 }
 console.log("Task 6:")
@@ -72,14 +66,14 @@ console.log(calculateBaseToExponent(10, 10));
 console.log(calculateBaseToExponent(3, 3));
 
 // Task 7
-const dogAge = (puppyAge) => {
+const dogAge = (puppyAge = 0) => {
     return puppyAge * 7;
 }
 console.log("Task 7:");
 console.log(`Your doggo is ${dogAge(4)} years old in human years!`);
 
 // Task 8 
-const calcSupply = (actualAge, amountPerDay) => {
+const calcSupply = (actualAge = 0, amountPerDay = 0) => {
     const maxage = 80;
     const remainingYears = maxage - actualAge;
     const totalAmountNeeded = ((remainingYears * 365) * amountPerDay);
