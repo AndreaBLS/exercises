ecd42e88c78c667218feae21c3fbcc7052d7fb17
AndreaBLS <frostbittenblash@gmail.com>
2020-06-15 14:34:01 +0200

    done

--- 29-PB-functions/29-index.js
+++ 29-PB-functions/29-index.js
@@ -68,7 +68,7 @@ console.log(calculateBaseToExponent(3, 3));
 // Task 7
 const humanYears = 7;
 const dogAge = (puppyAge = 0) =>
-    `Your doggo is ${puppyAge*humanYears} years old in human years!`
+    `Your doggo is ${puppyAge * humanYears} years old in human years!`
 
 console.log("Task 7:");
 console.log(dogAge(4));
@@ -85,9 +85,9 @@ console.log(calcSupply(25, 2));
 console.log(calcSupply(40, 3));
 
 // Task 9 
-const isWaldoHere = (string) => {
-    return string.includes('Waldo')
-}
+const isWaldoHere = (string) =>
+    string.includes('Waldo') || string.includes('waldo')
+
 console.log("Task 9:");
 console.log(isWaldoHere("is there wal here ?"));
 console.log(isWaldoHere("I found you Waldo!"));
@@ -95,3 +95,38 @@ console.log(isWaldoHere("is wally here?"));
 console.log(isWaldoHere("waldo is here"));
 
 // Task 10
+const equalSlices = (totalSlices, numberRecipients, slicesPerPerson) => numberRecipients * slicesPerPerson <= totalSlices;
+
+console.log("Task 10:")
+console.log(equalSlices(11, 5, 3));
+console.log(equalSlices(8, 3, 2));
+console.log(equalSlices(8, 3, 3));
+console.log(equalSlices(24, 12, 2));
+
+// Task 11
+const xO = (myString) => {
+    let checkForX = 0,
+        checkForO = 0,
+        myNewString = myString.toLowerCase();
+    for (let position = 0; position < myNewString.length; position++) {
+        if (myNewString.charAt(position) === "x") {
+            checkForX++;
+        } else if (myNewString.charAt(position) === "o") {
+            checkForO++;
+        }
+    }
+    if (checkForO === checkForX) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log("Task 11:");
+console.log(xO("ooxx"));
+console.log(xO("xooxx"));
+console.log(xO("ooxXm"));
+console.log(xO("zpzpzpp"));
+console.log(xO("zzoo"));
+
+// Task 12
