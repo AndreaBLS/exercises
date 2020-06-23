52549eb4cf8f19da00122f36ef7d60d70bbeff78
AndreaBLS <frostbittenblash@gmail.com>
2020-06-23 12:35:11 +0200

    ex 35 Task 1-4 done

--- 34-PB-arrays-pt2/34-andrea.js
+++ 34-PB-arrays-pt2/34-andrea.js
@@ -1,24 +1,51 @@
 "use strict";
 
-// Task 1
+// Task 1 : The Greater Numbers
 
-const findGreatest = ([], number) => {
-
+const findGreatest = (array, number) => {
+    let result = '';
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > number) {
+            result = result + array[i] + ' ';
+        }
+    }
+    return result;
 }
+console.log("Task 1 : The Greater Numbers");
+console.log(findGreatest([3, 4, 5], 4));
+console.log(findGreatest([10, 20, 30], 12));
+console.log(findGreatest([0, 10, 3], 4));
+
+// Task 2 : For the longest word
+
+const longestWord = (string) => {
+    let array = string.split(" "),
+        longest = "";
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > longest.length) {
+            longest = array[i];
+        }
+    } return longest;
+}
+console.log("Task 2 : For the longest word");
+console.log(longestWord('this is a web development course'));
+
+// Task 3
+
+const reverse = (numbers)
 
 
-
-
+console.log(reverse(34532));
 
 // Task 4 
 
-const findVowels = (string) => {
-    const vowels = str.match(/[aeiou]/gi);
-    return vowels;
+ const findVowels = (string) => {
+    const vowels = string.match(/[aeiou]/gi);
+    return vowels.length;
 }
 
 console.log("Task 04: AEIOU: Vowels");
-console.log(findVowels("this is a string"));
+console.log(findVowels("this is a string")); 
 
 
 // Task 5
---
+++ 36-PB-arrays-loops/36-andrea.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// Task 1: sumOfNumbers
+
+const sumOfNumbers = (array) => {
+    let result = null;
+    for (let i = 1; i < array.length; i++) {
+        result += array[i];
+    } return result;
+}
+console.log("Task 1: sumOfNumbers");
+console.log(sumOfNumbers([1, 2, 5, 10]));
+
+// Task 2: Hello Friends
+
+console.log("Task 2: Hello Friends");
+let friends = ["Jim", "Beam", "Johnny", "Walker"];
+for (let i = 0; i < friends.length; i++) {
+    console.log("Hello " + friends[i]);
+}
+
+// Task 3: City Names
+
+let cityNames = ["Berlin", "Paris", "Rome", "Prague"];
+let string = " ";
+for (let i = 0; i < cityNames.length; i++) {
+    string += cityNames[i] + ", ";
+}
+
+console.log("Task 3: City Names:", string);
+
+// Task 4: Odds and Evens
+
+const oddsEvens = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            array[i]--; // i was doing +1 but it doesnt work :( i would need to create a variable that contains array[i] + 1??
+        } else {
+            array[i]++;
+        }
+    } return array
+}
+
+console.log(oddsEvens([3, 5, 2, 4]));
+console.log(oddsEvens([6, 9, 10, 20]));
+
+// Task 5: Capitalize
+
