e7080bea77c216875c821f73d54c5a0c47f8994e
AndreaBLS <frostbittenblash@gmail.com>
2020-06-23 18:32:44 +0200

    ex 37 task1-2 done, finishing later

--- 36-PB-arrays-loops/36-andrea.js
+++ 36-PB-arrays-loops/36-andrea.js
@@ -25,20 +25,23 @@ let cityNames = ["Berlin", "Paris", "Rome", "Prague"];
 let string = " ";
 for (let i = 0; i < cityNames.length; i++) {
     string += cityNames[i] + ", ";
-}
+} string = string.substring(0, string.length - 2);
+
+// return array.join(',');
 
 console.log("Task 3: City Names:", string);
 
 // Task 4: Odds and Evens
 
 const oddsEvens = (array) => {
+    let newArray = [];
     for (let i = 0; i < array.length; i++) {
         if (array[i] % 2 === 0) {
-            array[i]--; // i was doing +1 but it doesnt work :( i would need to create a variable that contains array[i] + 1??
+            newArray.push(array[i] - 1);
         } else {
-            array[i]++;
+            newArray.push(array[i] + 1);
         }
-    } return array;
+    } return newArray;
 }
 console.log("Task 4: Odds and Evens");
 console.log(oddsEvens([3, 5, 2, 4]));
@@ -46,17 +49,17 @@ console.log(oddsEvens([6, 9, 10, 20]));
 
 // Task 5: Capitalize
 
-/* 
-
-not working
 const capitalize = (array) => {
-    toLower = array.toLowerCase();
+    let newArray = [];
     for (let i = 0; i < array.length; i++) {
-        toLower[i].charAt(0).toUppperCase()
+        newArray.push(array[i].charAt(0).toUpperCase() + array[i].toLowerCase().slice(1));
     }
-    return toLower
+    return newArray;
 }
 
+console.log("Task 5: Capitalize");
 console.log(capitalize(["matt", "sara", "lara"]));
 console.log(capitalize(["samuel", "MARIA", "luke", "mary"]));
-console.log(capitalize(["Cynthia", "Karen", "Jane", "Carrie"])); */
+console.log(capitalize(["Cynthia", "Karen", "Jane", "Carrie"]));
+
+// Task 6: No Duplicates!
---
+++ 37-PB-arrays-nested-loops/37-index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Task 1: Tree
+
+const halfTree = (character, lines) => {
+    let array = [];
+    for (let i = 0; i < lines; i++) {
+        for (let j = 0; j < i; j++) {
+            array.push(character);
+            console.log(array);
+        }
+    }
+}
+// not perfect but almost :D 
+
+console.log(halfTree("a", 4));
+
+// Task 2: 
+let arr = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+const verticalArrayPrinter = (array) => {
+   
+    for (let i = 0; i < array.length; i++) {
+        console.log(`Row ${i}`);
+        for (let j = 0; j < array[i].length; j++) {
+            console.log(arr[i][j]);
+        }
+    }
+}
+
+verticalArrayPrinter(arr);
+
+// Task
