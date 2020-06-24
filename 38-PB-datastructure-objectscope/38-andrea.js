25209916aad20b9b089d14313d523c3e70105e3f
AndreaBLS <frostbittenblash@gmail.com>
2020-06-24 15:27:51 +0200

    Ex 38 done

---
+++ 38-PB-datastructure-objectscope/38-andrea.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Task 1
+
+const student = {
+    firstName: "John",
+    lastName: "Smith",
+    class: 12,
+    myMethod: function () {
+        return `${this.firstName} ${this.lastName} is a student in class ${this.class}`;
+    }
+}
+
+console.log("Task 1: ");
+console.log(student.myMethod());
+
+// Task 2
+
+const person = {
+    firstName: "Johnny",
+    lastName: "Walker",
+    age: 12,
+    job: "developer",
+    city: "Berlin",
+    myMethod: function () {
+        return `${this.firstName} ${this.lastName} is a ${this.age} years old ${this.job} living in ${this.city}`;
+    }
+}
+
+console.log("Task 2:");
+console.log(person.myMethod());
