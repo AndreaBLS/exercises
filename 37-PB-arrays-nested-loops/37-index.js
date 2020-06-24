3c62a4a93767b1c6e4ca26213d564dc3898094db
AndreaBLS <frostbittenblash@gmail.com>
2020-06-24 09:17:28 +0200

    ex 37 task1-2 done

--- 37-PB-arrays-nested-loops/37-index.js
+++ 37-PB-arrays-nested-loops/37-index.js
@@ -11,13 +11,13 @@ const halfTree = (character, lines) => {
         }
     }
 }
-console.log("Task 1: Tree")
+console.log("Task 1: Tree");
 halfTree("*", 4);
 
 // Task 2: 
 let arr = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
 const verticalArrayPrinter = (array) => {
-   
+
     for (let i = 0; i < array.length; i++) {
         console.log(`Row ${i}`);
         for (let j = 0; j < array[i].length; j++) {
