095e5b2f41b5e2d5911261fa956532d878ebe41c
Blash <frostbittenblash@gmail.com>
2020-08-05 13:49:32 +0200

    ex 60 done

--- 59-Browser-modules-imports-exports/modules/index.js
+++ 59-Browser-modules-imports-exports/modules/index.js
@@ -1,1 +1,51 @@
 // Your code goes here!
+
+import {
+    modulo,
+    percentage,
+    percentageOf,
+    difference
+} from "./percentage.js"
+
+import {
+    fixRoundingErrors,
+    calculateAspectRatio
+} from "./aspect-ratio.js"
+
+const modulo1 = document.getElementById("modulo_1")
+const modulo2 = document.getElementById("modulo_2")
+const moduloResult = document.getElementById("modulo_result")
+
+modulo2.addEventListener("keyup", () => {
+    let modulo_result = modulo(modulo1.value, modulo2.value)
+    moduloResult.value = modulo_result
+})
+
+const percentage1 = document.getElementById("percentage_1")
+const percentage2 = document.getElementById("percentage_2")
+const percentageResult = document.getElementById("percentage_result")
+
+percentage2.addEventListener("keyup", () => {
+    let percentage_result = percentage(percentage1.value, percentage2.value)
+    percentageResult.value = percentage_result
+})
+
+const percentageOf1 = document.getElementById("percentageOf_1")
+const percentageOf2 = document.getElementById("percentageOf_2")
+const percentageOfResult = (document.getElementById("percentageOf_result"))
+
+percentageOf2.addEventListener("keyup", () => {
+    let percentageOf_result = percentage(percentageOf1.value, percentageOf2.value)
+    percentageOfResult.value = percentageOf_result
+
+})
+
+const difference1 = document.getElementById("difference_1")
+const difference2 = document.getElementById("difference_2")
+const differenceResult = document.getElementById("difference_result")
+
+difference2.addEventListener("keyup", () => {
+    let difference_result = difference(difference1.value, difference2.value)
+    differenceResult.value = difference_result
+})
+
--- 60-Browser-fetch-promises/src/scripts/index.js
+++ 60-Browser-fetch-promises/src/scripts/index.js
@@ -1,7 +1,26 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+/* import '../styles/main.scss'; */
 // Import any additional modules you want to include below \/
 
 
 // \/ All of your javascript should go here \/
+let myModal = document.querySelector(".modal")
+let mySpan = document.querySelector("span")
 
+let myPromise = new Promise(function (resolve, reject) {
+
+        resolve(setTimeout(() => {
+            myModal.style.display = "block"
+        }, 60000))
+
+        reject("Failed")
+
+    }).then(result => {})
+
+    .catch(message => {
+        message
+    })
+
+mySpan.addEventListener("click", () => {
+    myModal.style.display = "none"
+})
